8351338936389d2bf7793a7462f0179d377d303b
Junn <socker210@gmail.com>
2021-03-07 21:12:53 +0900

    Initial repo & Add array

---
+++ README.md
@@ -0,0 +1,5 @@
+// prettier
+// eslint
+// jest
+
+// webpack
---
+++ src/array/adt.ts
@@ -0,0 +1,21 @@
+export const create = <T>(n: number): Array<T> => {
+	const a: Array<T> = new Array(n)
+
+	return a
+}
+
+export const retrieve = <T>(a: Array<T>, i: number): T => {
+	if (i < 0 || i > a.length) {
+		throw new Error('Out of bounds!')
+	}
+
+	return a[i]
+}
+
+export const store = <T>(a: Array<T>, i: number, e: T): void => {
+	if (i < 0 || i > a.length) {
+		throw new Error('Out of bounds!')
+	}
+
+	a[i] = e
+}
